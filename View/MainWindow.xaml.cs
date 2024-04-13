68be0597c429a44e4a2eeac3bea709d4f7790859
quazistellar <gni7777@yandex.ru>
2024-04-13 20:04:09 +0300

    prikoli

---
+++ View/MainWindow.xaml.cs
@@ -0,0 +1,58 @@
+﻿using laba5_wpff.View;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace laba5_wpff
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        // переход на окно с кликером
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Clicker clicker = new Clicker();
+            clicker.ShowDialog();
+            
+        }
+
+        //переход на окно с угадай-число
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            GuessNumber guess = new GuessNumber();
+            guess.ShowDialog();
+        }
+
+        //переход на игру камень-ножницы-бумага
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            RockPapSciView rockPapSci = new RockPapSciView();
+            rockPapSci.ShowDialog();
+        }
+
+
+        //выход из проги
+        private void Button_Click_3(object sender, RoutedEventArgs e)
+        {
+            Environment.Exit(0);
+        }
+    }
+}
---
+++ ViewModel/ClickerViewModel.cs
@@ -0,0 +1,16 @@
+﻿using laba5_wpff.ViewModel.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace laba5_wpff.ViewModel
+{
+
+    internal class ClickerViewModel : BindingHelper
+    {
+
+    }
+
+}
